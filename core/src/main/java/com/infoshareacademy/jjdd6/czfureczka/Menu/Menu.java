813787be2b011f7792eb277c87a6617a418978f9
Piotr Wonotrski <p.wontorski@gmail.com>
2019-04-03 16:45:47 +0200

    dodany plik autobusu

--- core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/Menu/Menu.java
+++ core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/Menu/Menu.java
@@ -1,22 +1,31 @@
 package com.infoshareacademy.jjdd6.czfureczka.Menu;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Menu {
 
-    public static int menu(){
-        System.out.println();
-        System.out.println("     ****************************************");
-        System.out.println("     *                 MENU                 *");
-        System.out.println("     ****************************************");
-        System.out.println("     1. Suma");
-        System.out.println("     2. Sinus");
-        System.out.println("     3. Informaja");
-        System.out.println("     0. Koniec");
+    public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        int w = in.nextInt();
 
-        return w;
+        File plikAutobus = new File("baner.txt");
+        Scanner odczyt = new Scanner(new File());
+        Sout
+
+        public static int menu () {
+            System.out.println();
+            System.out.println("     ****************************************");
+            System.out.println("     *                 MENU                 *");
+            System.out.println("     ****************************************");
+            System.out.println("     1. Suma");
+            System.out.println("     2. Sinus");
+            System.out.println("     3. Informaja");
+            System.out.println("     0. Koniec");
+
+            Scanner in = new Scanner(System.in);
+            int w = in.nextInt();
+
+            return w;
+        }
     }
 }
