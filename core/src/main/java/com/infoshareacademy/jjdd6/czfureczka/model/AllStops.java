0070a14abd52602a163615ea584ddf43016b8f82
Paula Madej <paula.miromad@gmail.com>
2019-04-05 16:34:17 +0200

    Zmiany

--- core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/model/AllStops.java
+++ core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/model/AllStops.java
@@ -4,6 +4,7 @@ import com.infoshareacademy.jjdd6.czfureczka.repository.Repository;
 
 import java.awt.*;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -14,26 +15,44 @@ public class AllStops {
                 .collect(Collectors.toList()).get(0);
         StopsWithDate stops = Repository.getInstance().stops.get(s);
 
-      /*  List<Stop> newStops = stops.getStops();
-        newStops.stream()
-                .filter(s1->s1.getNonpassenger()==0)
+        Stop stop1 = stops.getStops().get(0);
+
+        List<Stop> newStops = stops.getStops();
+
+        List<String> allStopsss=newStops.stream()
+                .filter(s1 -> s1.getNonpassenger() == 0)
                 .map(Stop::getStopDesc)
                 .distinct()
                 .sorted()
-                .forEach(System.out::println);
-       */
-        List<Stop> newStopId=stops.getStops();
-        newStopId.stream()
-                .filter(s1->s1.getNonpassenger()==0)
-                .map(Stop::getStopId)
+                .collect(Collectors.toList());
+        System.out.println(allStopsss.get(0));
+
+
+
+        List<Stop> stooooop=newStops.stream()
+                .filter(s1 -> s1.getNonpassenger() == 0)
+                .flatMap()
+
+
+// Budapesztańska
+
+        List<Integer> allStopsIds = newStops.stream()
+                .filter(s1 -> s1.getNonpassenger() == 0)
+                .map(o -> o.getStopId())
                 .sorted()
-                .collect(Collectors.toList())
-                .forEach(System.out::println);
+                .collect(Collectors.toList());
+        System.out.println(allStopsIds);
 
 
-        System.out.println("-----------------------------");
+       for (int i=0; i< allStopsss.size(); i++){
+
+           if("Budapesztańska".equals(allStopsss.get(i))){
+               System.out.println("aaa");
+
+       }
 
 
+      }
     }
 
 }
