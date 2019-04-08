be1005eae45bce268a4b557574031c242619e348
Piotr Wonotrski <p.wontorski@gmail.com>
2019-04-08 16:26:57 +0200

    kolejny commit 08.04.2019

--- core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/Menu/MenuMain.java
+++ core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/Menu/MenuMain.java
@@ -23,29 +23,65 @@ public class MenuMain {
 
         System.out.println(" ");
         System.out.println("Witaj w aplikacji Szybko do Celu! Dzisiaj na pewno dojedziesz na czas :) ");
-        System.out.println("              Data: "+today+" "+"Godzina: "+now+"                      ");
+        System.out.println("              Data: " + today + " " + "Godzina: " + now + "                      ");
         System.out.println("_________________________________________________________________________");
+
+        System.out.println("Wybierz opcje:");
+        System.out.println("1.Odjazd z przystanku.");
+        System.out.println("2.Relacja przystanek-przystanek.");
+        System.out.println(" ");
+
         Scanner scan = new Scanner(System.in);
-        System.out.print("1. Wpisz przystanek startowy: ");
-        String startowy =scan.nextLine();
-        Scanner scan2 = new Scanner(System.in);
-        System.out.print("2. Wpisz przystanek docelowy: ");
-        String docelowy =scan.nextLine();
-        System.out.println("Relacja:"+startowy+"-"+docelowy);
-        System.out.println("Dostepne polaczenia:");
-        Scanner scan3 = new Scanner(System.in);
-        System.out.print("Wybierz polaczenie:");
-        Integer polaczenie = scan.nextInt();
+        System.out.print("Twoj wybor:");
+        Integer wybor = scan.nextInt();
 
+        int number = wybor;
+        switch (number) {
 
+            case 1:
+                System.out.print("Wpisz przystanek startowy: ");
+                System.out.println(" ");
 
+                Scanner scan2 = new Scanner(System.in);
+                System.out.println("Przystenk startowy:");
+                String startowy = scan.nextLine();
+                System.out.println("Dostepne polacznia:");
 
+                Scanner scan3 = new Scanner(System.in);
+                System.out.print("Wybierz polaczenie:");
+                Integer polaczenie = scan3.nextInt();
 
+            case 2:
 
+                Scanner scan4 = new Scanner(System.in);
+                System.out.println("Wpisz przystanek startowy: ");
+                String start = scan.nextLine();
 
+                Scanner scan5 = new Scanner(System.in);
+                System.out.println("Wpisz przystanek docelowy: ");
+                String docelowy = scan.nextLine();
+
+                System.out.println("Relacja:" + start + "-" + docelowy);
+
+                System.out.println("Dostepne polaczenia:");
+
+                Scanner scan6 = new Scanner(System.in);
+                System.out.print("Wybierz polaczenie:");
+                Integer polaczenie2 = scan.nextInt();
+
+                int number2 = polaczenie2;
+                switch (number2) {
+                    case 1:
+                        System.out.println("Najblizszy odjazd: ");
+                    case 2:
+                        System.out.println("Najblizszy odjazd: ");
+                    case 3:
+                        System.out.println("Najblizszy odjazd: ");
+                }
 
 
         }
+    }
 }
 
 
