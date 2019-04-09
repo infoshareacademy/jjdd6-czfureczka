86b3bb361b8096859dfab77fa59ff046565a84f9
Piotr Wonotrski <p.wontorski@gmail.com>
2019-04-09 14:47:57 +0200

    poprawki 09.04.2019

--- core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/Menu/Menu.java
+++ core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/Menu/Menu.java
@@ -1,4 +1,21 @@
 package com.infoshareacademy.jjdd6.czfureczka.Menu;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Menu {
+
+    public void run(){
+
+        LocalDate today = LocalDate.now();
+        LocalTime now = LocalTime.now();
+
+        System.out.println(" ");
+        System.out.println("Witaj w aplikacji 'Szybko do Celu'! Dzisiaj na pewno dojedziesz na czas :)");
+        System.out.println("               Data: "+today+" "+"Godzina: "+now );
+        System.out.println("__________________________________________________________________________");
+
+
+
+    }
 }
