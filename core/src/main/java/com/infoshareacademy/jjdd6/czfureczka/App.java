680d60fb9e565f0d8d1f8d417c243f26e14c876f
RemikA <11580041+dijx@users.noreply.github.com>
2019-04-10 15:36:51 +0200

    Merge

--- core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/App.java
+++ core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/App.java
@@ -12,11 +12,6 @@ public class App {
             System.err.println("Data could not be loaded");
             return;
         }
-/*
-        AllStops allStops=new AllStops();
-        allStops.run();
-*/
-
     }
 
 
