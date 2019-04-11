ed2b1850f20f201f97a683e62cca76da6512cced
Paula Madej <paula.miromad@gmail.com>
2019-04-11 16:58:09 +0200

    data

--- core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/App.java
+++ core/src/main/java/com/infoshareacademy/jjdd6/czfureczka/App.java
@@ -1,7 +1,14 @@
 package com.infoshareacademy.jjdd6.czfureczka;
 
 
+import com.infoshareacademy.jjdd6.czfureczka.repository.Repository;
 import com.infoshareacademy.jjdd6.czfureczka.repository.RepositoryLoader;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
@@ -12,6 +19,25 @@ public class App {
             System.err.println("Data could not be loaded");
             return;
         }
+
+        List<Integer> data = Repository.getInstance().getStopTimes().keySet().stream()
+                .collect(Collectors.toList());
+
+        //  data.forEach(System.out::println);
+
+        List<Integer> neww = Arrays.asList(2, 12);
+
+      //  neww.forEach(System.out::println);
+
+     //Stream.of(data,neww).filter(s,b)->neww.contains(data)).forEach(System.out::println);
+
+data.retainAll(neww);
+System.out.println(data);
+
+       List<Integer> routShortNames = data.stream()
+                .filter(s-> data.contains(neww))
+                .collect(Collectors.toList());
+        routShortNames.forEach(System.out::println);
 
 
     }
